817aa8062f1e19601bc8840f5a98f81569d083c8
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-20 03:02:21 +0200

    Part 01 : Theoretical Questions

--- OOP_02/Program.cs
+++ OOP_02/Program.cs
@@ -3,6 +3,30 @@
 
 internal class Program
 {
+    #region  Part 01 : Theoretical Questions 
+    //----------------------QA-----------------------------
+    /* The Difference between class and struct
+     * 
+     * 1. Type : Class is reference type , Struct is value type 
+     * 
+     * 2. Stored in : Class stored in heap , Struct stored in stack 
+     * 
+     * 3. inheritance : Class supports that , Struct doesn't support
+     * 
+     * 4. Default Constructor : Class Provided if none defined (implicit Parameterless ) , Struct the parameterless constructor always exist 
+     * 
+     * 5. null : Class can be null , Struct can not be null unless the ' ? '(nullable) operator
+     * 
+     * 6. Best for : Class with Complex data with behavior , inheritance , shared data , Struct with simple and small data
+     */
+
+    //----------------------QB-----------------------------
+    /*
+     As class supports inheritance and behavior in classes are more Professional (u can use override , overload , interfaces , ....)
+     */
+
+    #endregion
+
     static public void Main()
     {
       
